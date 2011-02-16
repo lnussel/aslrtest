46bc8c8a2d4f2bc91ef66888f2619e4e04303d19
Ludwig Nussel <ludwig.nussel@suse.de>
2011-02-16 16:05:11 +0100

    some tuning

--- Makefile
+++ Makefile
@@ -1,6 +1,11 @@
-CFLAGS=-Wall -g -O2 -fpie -pie -m32
+ARCH=$(shell uname -m | sed -e s/i.86/i386/)
+CFLAGS=-Wall -g -O2 -fpie -pie
 LDLIBS=-ldl
 SAMPLES=3333
+
+ifeq ($(ARCH),i386)
+	CFLAGS+=-m32
+endif
 
 bins=libcaddr mainaddr
 
@@ -9,8 +14,9 @@ all: $(bins:%=%.png)
 $(bins:%=%.data): %.data: %
 	./gensome $* $(SAMPLES) > $*.data
 
-$(bins:%=%.png): %.png: %.data plot.gpl
-	./plot $*
+$(bins:%=%.png): %.png: %.data
+	./analyze.pl --out $*.stats --gnuplot $*.gpl $*.data
+	gnuplot $*.gpl
 
 clean:
 	rm -f $(bins:%=%.png) $(bins:%=%.data) $(bins:%=%.stats) $(bins)
--- analyze.pl
+++ analyze.pl
@@ -4,9 +4,28 @@ use strict;
 my $min;
 my $cnt = 0;
 
-die "Usage: $0 file" unless @ARGV;
+my $ofile = "/dev/stdout";
+my $gnuplot;
 
-open(F, '<', $ARGV[0]) or die "$!\n";
+while (@ARGV) {
+	last if $ARGV[0] eq '--';
+	if ($ARGV[0] eq '--out') {
+		shift @ARGV;
+		$ofile = shift @ARGV || die "missing argument";
+		next;
+	}
+	if ($ARGV[0] eq '--gnuplot') {
+		shift @ARGV;
+		$gnuplot = shift @ARGV || die "missing argument";
+		next;
+	}
+	last;
+}
+
+die "Usage: $0 file" unless @ARGV && -e $ARGV[0];
+
+my $file = shift @ARGV;
+open(F, '<', $file) or die "$!\n";
 while(<F>)
 {
 	chomp;
@@ -23,13 +42,43 @@ while(<F>)
 	$a{$v} ||= 0;
 	++$a{$v};
 }
+close F;
 
+die "$!" unless open(O, '>', $ofile);
 my $na = keys %a;
-print "# samples: $cnt\n";
-print "# addresses: $na\n";
-print "# base address: $min\n";
-print "# address count share\n";
-while (my ($k, $v) = each %a) {
+print O "# samples: $cnt\n";
+print O "# addresses: $na\n";
+print O "# base address: $min\n";
+print O "# address count share\n";
+for my $k (sort {$a{$a} <=> $a{$b}} keys %a) {
+	my $v = $a{$k};
 	my $p = $v/$cnt;
-	print "$k\t$v\t$p\n";
+	print O "$k\t$v\t$p\n";
 }
+close O;
+
+if ($gnuplot) {
+	die "$!" unless open(O, '>', $gnuplot);
+	my $exp = 1/$na * 100;
+	my $title = sprintf ("%d samples, %d addresses", $cnt, $na);
+	my $img = $ofile;
+	$img =~ s/\..*$/.png/;
+	while (<DATA>) {
+		s/\$img/$img/g;
+		s/\$title/$title/g;
+		s/\$data/$ofile/g;
+		s/\$exp/$exp/g;
+		print O;
+	}
+	close O;
+}
+
+__END__
+set terminal png
+set output "$img"
+set title "$title"
+set xlabel "Address"
+set ylabel "Probability in %"
+set yrange [0:*]
+set xrange [0:*]
+plot "$data" using ($3*100) with steps title "actual", $exp title "ideal"
